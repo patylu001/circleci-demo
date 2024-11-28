d1cbb3351857757351a9dff94eb274486a0f7625
lLeonxg <156732929+lLeonxg@users.noreply.github.com>
2024-11-28 17:57:13 -0600

    Update main-test.py

--- main-test.py
+++ main-test.py
@@ -3,7 +3,7 @@ from main import Add
 
 def TestAdd():
         assert Add(2,3) == 5
-        assert Add(5,5) == 10
+        assert Add(2,5) == 10
         assert Add(10,10) == 20
         print("Add Function works correctly")
 
